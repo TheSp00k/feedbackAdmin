a38ebe6941555bc317d43f187c781f6271151a98
Kestas <k.tautvydas@gmail.com>
2017-08-17 00:56:41 +0300

    dizainas

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -11,14 +11,26 @@ import {NotificationService} from "../shared/utils/notification.service";
 export class SettingsComponent implements OnInit {
     elementRef:ElementRef;
     public client;
+    public dropdownOpen = false;
 	public currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    public requestDelayDays = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
 
     onPicked(time:Date) {
         this.client.requesttime = time;
     };
-
+    
     onSpin(days:number) {
         this.client.requestdelay = days;
+    };
+
+    public toggleDropdown = () => {
+        this.dropdownOpen = this.dropdownOpen ? false : true;
+    };
+    
+    public selectDropdown = (option) => {
+        // event.stopPropagation();
+        this.client.requestdelay = option;
+        this.dropdownOpen = false;
     };
 
     public saveClientSettings = () => {
